575929f05f05190e954b58d0a98ef8f23fe6f6d0
irispunt <iris.de.graaf@live.nl>
2019-04-24 09:41:36 +0200

    eerste versie

---
+++ opdracht1/v1/js/script.js
@@ -0,0 +1,6 @@
+var button = document.querySelector('.meerinfo');
+var hidden = document.querySelector('.hidden');
+
+button.addEventListener('click', function () {
+    hidden.classList.toggle('hidden');
+});
