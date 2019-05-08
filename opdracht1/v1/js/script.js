8b89adc2d90cc60e5fd26e00b71c59af4c3e8a70
irispunt <iris.de.graaf@live.nl>
2019-05-08 09:36:12 +0200

    v2

--- opdracht1/v1/js/script.js
+++ opdracht1/v1/js/script.js
@@ -1,6 +1,11 @@
-var button = document.querySelector('.meerinfo');
-var hidden = document.querySelector('.hidden');
+var button = document.querySelectorAll('.meerinfo');
+var hidden = document.querySelectorAll('.hidden');
 
-button.addEventListener('click', function () {
-    hidden.classList.toggle('hidden');
-});
+
+for (var i = 0; i < button.length; i++) {
+
+    button[i].addEventListener('click', function () {
+        //console.log(this.parentNode.nextElementSibling);
+        this.parentNode.nextElementSibling.classList.toggle('show');
+    });
+}
